7b772441bfcf1a3ddf9fd8513f3203fe8a7c9e31
Rafael Marreca <rafael145a@gmail.com>
2025-04-14 14:11:00 -0300

    improve active status

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -6,7 +6,9 @@ import { useState } from "react";
 import { AiOutlineDashboard } from "react-icons/ai";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineKeyboardDoubleArrowLeft } from "react-icons/md";
-
+import { FaChartGantt } from "react-icons/fa6";
+import { MdOutlineGroups2 } from "react-icons/md";
+import { IoSettings } from "react-icons/io5";
 
 
 
@@ -16,9 +18,9 @@ export const Sidebar = () => {
     const [collapsed, setCollapsed] = useState(false);
     const menuItems = [
         { name: "Dashboard", href: "/dashboard", icon: <AiOutlineDashboard size={28} /> },
-        { name: "Projects", href: "/projects", icon: <AiOutlineDashboard size={28} /> },
-        { name: "Team", href: "/team", icon: <AiOutlineDashboard size={28} /> },
-        { name: "Settings", href: "/settings", icon: <AiOutlineDashboard size={28} /> },
+        { name: "Projects", href: "/projects", icon: <FaChartGantt size={28} /> },
+        { name: "Team", href: "/team", icon: <MdOutlineGroups2 size={28} /> },
+        { name: "Settings", href: "/settings", icon: <IoSettings size={28} /> },
     ];
 
     return (
@@ -27,26 +29,27 @@ export const Sidebar = () => {
             className={`h-screen ${!collapsed ? "w-64" : "w-16 [&>header]:px-0"} flex-col justify-start rounded-none max-sm:w-full max-sm:h-auto border bg-zinc-300 transition-all ease-in-out `}
             isMenuOpen={isMenuOpen}
             onMenuOpenChange={setIsMenuOpen}
+            
         >
-            <NavbarContent className="sm:hidden" justify="start">
-                <NavbarMenuToggle aria-label="Open menu" />
+            <NavbarContent className="sm:hidden pl-4" justify="start">
+                <NavbarMenuToggle aria-label="Open menu" icon={<GiHamburgerMenu size={28} />}  />
                 <NavbarBrand>
-                    <p className="font-bold text-inherit">My App</p>
+                    <p className="font-bold text-inherit">My App Sidebar</p>
                 </NavbarBrand>
             </NavbarContent>
 
             {!collapsed ?
-                <NavbarContent className="hidden sm:flex sm:flex-col items-start pt-4 gap-4" justify="start">
+                <NavbarContent className="hidden sm:flex sm:flex-col items-start pt-4" justify="start">
                     <NavbarBrand className="mb-6 flex items-center justify-between w-full">
-                        <p className="font-bold text-inherit">My App</p>
+                        <p className="font-bold text-inherit">My App Sidebar</p>
                         <Button onPress={() => setCollapsed(true)} className="bg-transparent"><MdOutlineKeyboardDoubleArrowLeft size={24} /></Button>
                     </NavbarBrand>
 
                     {menuItems.map((item, index) => (
-                        <NavbarItem key={index} className="w-full" isActive={pathname === item.href}>
+                        <NavbarItem key={index} className="w-full rounded-md data-[active]:bg-default-100 data-[active]:py-2  transition-all" isActive={pathname === item.href}>
                             <Link
                                 href={item.href}
-                                className="w-full px-4 py-2 rounded hover:bg-default-100"
+                                className="w-full px-4 py-2 rounded hover:bg-default-100 transition-colors"
                             >
                                 {item.name}
                             </Link>
@@ -60,7 +63,7 @@ export const Sidebar = () => {
                     </NavbarBrand>
 
                     {menuItems.map((item, index) => (
-                        <NavbarItem key={index} className="w-full flex items-center align-center data-[active]:bg-white transition-colors" isActive={pathname === item.href}>
+                        <NavbarItem key={index} className="w-full flex items-center align-center data-[active]:bg-default-100 transition-colors" isActive={pathname === item.href}>
                             <Link
                                 href={item.href}
                                 className="w-full px-4 py-2 rounded hover:bg-default-100 transition-colors"
@@ -74,7 +77,7 @@ export const Sidebar = () => {
 
             <NavbarMenu className="pt-4">
                 {menuItems.map((item, index) => (
-                    <NavbarMenuItem key={index}>
+                    <NavbarMenuItem key={index} className="data-[active]:bg-default-100 transition-all data-[active]:p-2 rounded-md" isActive={pathname === item.href}>
                         <Link
                             href={item.href}
                             className="w-full"
